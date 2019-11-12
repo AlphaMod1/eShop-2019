6efa3c59f47b3380bd24417729e34a692f9a36df
DeividasBa <deividas.barbsys25@gmail.com>
2019-11-12 17:09:54 +0200

    Vendor basic #23
    HTML, JS basics, CSS

---
+++ Vendor/js/data.js
@@ -0,0 +1,47 @@
+const data = [
+    {
+        order:'1230',
+        laikas: '12:30',
+        vardas: 'Rimantas',
+        telefonas: '865528555',
+        status: 'ready',
+    },
+    {
+        order:'1231',
+        laikas: '12:30',
+        vardas: 'Rimantas',
+        telefonas: '865528555',
+        status: 'ready',
+    },
+    {
+        order:'1232',
+        laikas: '12:30',
+        vardas: 'Rimantas',
+        telefonas: '865528555',
+        status: 'ready',
+    },
+    {
+        order:'1233',
+        laikas: '12:30',
+        vardas: 'Rimantas',
+        telefonas: '865528555',
+        status: 'ready',
+    },
+    {
+        order:'1234',
+        laikas: '12:30',
+        vardas: 'Rimantas',
+        telefonas: '865528555',
+        status: 'ready',
+    },
+    {
+        order:'1235',
+        laikas: '12:30',
+        vardas: 'Rimantas',
+        telefonas: '865528555',
+        status: 'ready',
+    },
+];
+
+renderTable(data);
+
