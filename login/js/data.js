517e007547429e739b1663cfc45ef5755220fc44
AlphaMod1 <liutauras.vijeikis@gmail.com>
2019-11-18 16:04:40 +0200

    added display name

--- login/js/data.js
+++ login/js/data.js
@@ -3,20 +3,24 @@ const login_info = [
         email:"vendort1@mail.com",
         password:"cGFzc3dvcmQrKys=",
         id:"t1",
+        username:"Burito Republic",
     },
     {
         email:"vendort2@mail.com",
         password:"cGFzc3dvcmQrKys=",
         id:"t2",
+        username:"John\'s burgers",
     },
     {
         email:"vendort3@mail.com",
         password:"cGFzc3dvcmQrKys=",
         id:"t3",
+        username:"Yoko sushi",
     },
     {
         email:"aaa@aaa.com",
         password:"cGFzc3dvcmQrKys=",
         id:"test",
+        username:"Test user",
     },
 ];
