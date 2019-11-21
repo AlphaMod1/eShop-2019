bb8b722e12b85027ae3f9c1e6a514df406e07987
AlphaMod1 <liutauras.vijeikis@gmail.com>
2019-11-21 10:31:37 +0200

    generation

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,5 +1,5 @@
 updateHeaderTime();
-loadCont('dashboard');
+loadCont('edit');
 
 function updateHeaderTime() {
 
@@ -21,8 +21,13 @@ function updateHeaderTime() {
 }
 
 function loadCont(loc) {
+    changeCurrentContTitle(loc);
     changeBtn_LeftMenu(loc);
     renderContent(loc);
+}
+
+function changeCurrentContTitle(loc) {
+    return document.getElementById('current-container').innerHTML = loc;
 }
 
 function changeBtn_LeftMenu(loc) {
@@ -34,11 +39,114 @@ function changeBtn_LeftMenu(loc) {
     btn.classList.add("left_list-item-active");
 }
 
-function renderContent(loc){
-    let content = document.querySelector('#content > .cont-'+loc);
+function renderContent(loc) {
+    let content = document.querySelector('#content > .cont-' + loc);
     try {
         document.querySelector('.show-container').classList.remove("show-container");
     }
     catch{ }
     content.classList.add("show-container");
+
+    try {
+        let rm = document.querySelectorAll('.destroy');
+        Array.prototype.forEach.call(rm, function (node) {
+            node.parentNode.removeChild(node);
+        });
+    }
+    catch { }
+
+    switch (loc) {
+        case "dashboard":
+            renderFor_dashboard();
+            break;
+        case "users":
+            renderFor_users();
+            break;
+        case "vendors":
+            renderFor_vendors();
+            break;
+        case "edit":
+            renderFor_edit();
+            break;
+        case "inbox":
+            renderFor_inbox();
+            break;
+        default:
+
+            break;
+    }
+}
+
+
+function renderFor_dashboard() {
+    //insert data
+}
+
+
+function renderFor_users() {
+    let table = document.querySelector('#users-table');
+    let HTML;
+    for (let i = 0; i < fakeUsers.length; i++) {
+        HTML = '';
+        HTML += `<tr class='destroy'>
+        <td>${i}</td>
+        <td>${fakeUsers[i].name}</td>
+        <td>${fakeUsers[i].email}</td>
+        <td>${fakeUsers[i].tel}</td>
+        </tr>`;
+        table.insertAdjacentHTML('beforeend', HTML);
+    }
+}
+
+function renderFor_vendors() {
+    let table = document.querySelector('#vendor-table');
+    let HTML;
+    for (let i = 0; i < trucks.length; i++) {
+        HTML = '';
+        HTML += `<tr class='destroy'>
+        <td>${i}</td>
+        <td>${trucks[i].name}</td>
+        <td>${trucks[i].email}</td>
+        <td>${trucks[i].tel}</td>
+        </tr>`;
+        table.insertAdjacentHTML('beforeend', HTML);
+    }
+}
+
+function renderFor_edit() {
+    let table = document.querySelector('#edit-table');
+    let HTML;
+    for (let i = 0; i < trucks.length; i++) {
+        HTML = '';
+        HTML += `<tr class='destroy'>
+        <td>${i}</td>
+        <td>${trucks[i].name}</td>
+        <td>${trucks[i].email}</td>
+        <td>${trucks[i].tel}</td>
+        <td class='btn-delete'>Delete</td>
+        </tr>`;
+        table.insertAdjacentHTML('beforeend', HTML);
+    }
+}
+
+function renderFor_inbox() {
+    let table = document.querySelector('#inbox-table');
+    let HTML;
+    for (let i = 0; i < trucks.length; i++) {
+        HTML = '';
+        HTML += `<tr class='destroy'>
+        <td>${inbox[i].name}</td>
+        <td>${inbox[i].email}</td>
+        <td class='btn-open'>Open</td>
+        </tr>`;
+        table.insertAdjacentHTML('beforeend', HTML);
+    }
+}
+
+function addNewUserModal(){
+
+}
+
+function sendMailModal(){
+
 }
