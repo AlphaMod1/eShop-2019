1b945e06b8cc95a63af26ef655bcc5f1c80e1b57
martynasvitenas <martynas.vitenas@gmail.com>
2019-11-22 10:27:19 +0200

    Receipt

---
+++ Receipt/JS/actions.js
@@ -0,0 +1,21 @@
+// Header
+
+genHeaderDropdownTrucks();
+// header end
+
+// Overlay 
+const ovrl = document.querySelector('#receipt .issues');
+const faq = document.querySelector('#receipt');
+const close = document.querySelector('#receipt .overlay');
+
+ovrl.addEventListener('click', function(){
+    return faq.classList.add('showOverlay');
+});
+
+document.addEventListener('keypress', ()=>{ 
+    return faq.classList.remove('showOverlay');
+});
+
+close.addEventListener('click', () => {
+    faq.classList.remove('showOverlay');
+});
