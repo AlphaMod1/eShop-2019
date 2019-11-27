feb12830e859d213eaf61fdebf81d6d3ae798ea2
AlphaMod1 <liutauras.vijeikis@gmail.com>
2019-11-27 14:38:38 +0200

    food trucks drop down now actually works

--- js/function.js
+++ js/function.js
@@ -13,7 +13,7 @@ function genHeaderDropdownTrucks() {
     let dropdown = document.getElementById('dropdown-trucks');
     let HTML = '';
     for (let i = 0; i < trucks.length; i++) {
-        HTML += `<a href="#">${trucks[i].name}</a>`;
+        HTML += `<a href="../index.html#dropdownToSpecialOffers">${trucks[i].name}</a>`;
     }
     dropdown.innerHTML = HTML;
 }
